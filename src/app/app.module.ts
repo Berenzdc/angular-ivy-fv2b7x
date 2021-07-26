f3333d4c01fd72700b21172337ab549b7f5d5225
Berenzdc <berenzdc@gmail.com>
2021-07-26 13:20:37 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
+import { PaginationComponent } from './pagination/pagination.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, InfoBoxComponent, PaginationComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/verfahren.ts
@@ -0,0 +1,32 @@
+export interface verfahren {
+  id:number;
+  name:string;
+  verantwortlicher:string;
+}
+
+export const verfahren = [
+  {
+    id: 1,
+    name: 'Java',
+    verantwortlicher: 'Benjamin Seewald'
+ 
+  },
+  {
+    id: 2,
+    name: 'test1',
+    verantwortlicher: 'Benjamin Seewald'
+ 
+  },
+  {
+    id: 3,
+    name: 'zulangfürdiebox',
+    verantwortlicher: 'Benjamin Seewald'
+ 
+  },
+  {
+    id: 4,
+    name: 'test3',
+    verantwortlicher: 'Benjamin Seewald'
+ 
+  }
+];
